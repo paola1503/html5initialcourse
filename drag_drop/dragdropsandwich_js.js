aaff070c55bfad269cdeaa2140a951abfa97cfe0
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-05 18:13:40 +0100

    Update dragdropsandwich_js.js

--- drag_drop/dragdropsandwich_js.js
+++ drag_drop/dragdropsandwich_js.js
@@ -8,19 +8,22 @@ function start(){
 	elem_origin.addEventListener("dragstart",start_drag,false);
 	
 	elem_destination=document.getElementById("destinationarea");
-	elem_destination.addEventListener("dragenter",function(e){
-	e.preventDefault();},false);
-	
 	elem_destination.addEventListener("dragover",function(e){
 	e.preventDefault();},false);
 	
-	elem_destination.addEventListener("drop",dropped,false);
+	elem_origin.addEventListener("dragend",finished,false);
 	elem_destination.addEventListener("drop",function(){alert("Don't disturb me when I'm eating");},false);
+	elem_destination.addEventListener("drop",dropped,false);
 	}
 	
 function start_drag(e){
 	var code="<img src='"+elem_origin.getAttribute("src")+"'>";
 	e.dataTransfer.setData("Text",code);
+	}
+	
+function finished(e){
+	var element=e.target;
+	element.style.visibility="hidden";
 	}
 	
 function dropped(e){
