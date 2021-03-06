cb2f44a4e8fc21c86ef27705cd8e9ae41620346e
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-06 12:37:11 +0100

    Create dragdropfruits_js.js

---
+++ drag_drop/dragdropfruits_js.js
@@ -0,0 +1,48 @@
+//JavaScript Document
+
+var elem_destination;
+
+function start(){
+	var images=document.querySelectorAll("#imagebox img");
+	for (var i=0;i<images.length;i++){
+		images[i].addEventListener("dragstart",start_drag,false);
+		}
+		
+	elem_destination=document.getElementById("destinationarea");
+	elem_destination.addEventListener("dragenter",entering,false);
+	elem_destination.addEventListener("dragover",function(e){
+		e.preventDefault();},false);
+	elem_destination.addEventListener("drop",dropped,false);
+	elem_destination.addEventListener("dragleave",leaving,false);
+	}
+		
+function start_drag(e){
+	var element=e.target;
+	e.dataTransfer.setData("Text",element.getAttribute("id"));
+	}
+	
+function entering(e){
+	e.preventDefault();
+    elem_destination.style.background="rgba(8,252,25,.8)";
+
+	}
+	
+function dropped(e){
+	e.preventDefault();
+	var id=e.dataTransfer.getData("Text");
+	if(id!="icecream"){
+    	var src=document.getElementById(id).src;
+        elem_destination.innerHTML="<img src='"+ src +"'>";
+        }
+    else{
+        elem_destination.innerHTML="That is not a fruit, try again!";
+        elem_destination.style.background="#FA0D29";
+        }
+	}
+	
+function leaving(e){
+	e.preventDefault();
+	elem_destination.style.background="#FFFFFF";
+	}
+
+window.addEventListener("load",start,false);
