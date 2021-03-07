7d5e0a861b57e5263a616b310329165c66e864ea
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-07 09:32:14 +0100

    Update dragdropfruits_js.js

--- drag_drop/dragdropfruits_js.js
+++ drag_drop/dragdropfruits_js.js
@@ -6,6 +6,7 @@ function start(){
 	var images=document.querySelectorAll("#imagebox img");
 	for (var i=0;i<images.length;i++){
 		images[i].addEventListener("dragstart",start_drag,false);
+		images[i].addEventListener("dragend",end_drag,false);
 		}
 		
 	elem_destination=document.getElementById("destinationarea");
@@ -18,21 +19,25 @@ function start(){
 		
 function start_drag(e){
 	var element=e.target;
-	e.dataTransfer.setData("Text",element.getAttribute("id"));
+	e.dataTransfer.setData("text",element.getAttribute("id"));
 	}
 	
 function entering(e){
 	e.preventDefault();
     elem_destination.style.background="rgba(8,252,25,.8)";
-
+	}
+	
+function end_drag(e){
+	var element=e.target;
+	element.style.visibility="hidden";
 	}
 	
 function dropped(e){
 	e.preventDefault();
-	var id=e.dataTransfer.getData("Text");
+	var id=e.dataTransfer.getData("text");
 	if(id!="icecream"){
     	var src=document.getElementById(id).src;
-        elem_destination.innerHTML="<img src='"+ src +"'>";
+        elem_destination.innerHTML="<img src='"+ src +"' height='200'>";
         }
     else{
         elem_destination.innerHTML="That is not a fruit, try again!";
