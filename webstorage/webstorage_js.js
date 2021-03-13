b671b16b11fc1d158c8b5ae2f0c73fd29c451daa
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-13 20:04:03 +0100

    Update webstorage_js.js

--- webstorage/webstorage_js.js
+++ webstorage/webstorage_js.js
@@ -2,20 +2,39 @@
 
 function start(){
 	var button=document.getElementById("save")
-	button.addEventListener("click",newitem,false);
+	button.addEventListener("click",new_item,false);
 	}
 	
-function newitem(){
+function new_item(){
 	var key=document.getElementById("key").value;
 	var value=document.getElementById("value").value;
 	sessionStorage.setItem(key,value);
 	read_show(key);
+	document.getElementById("key").value="";
+	document.getElementById("value").value="";
 	}
 	
 function read_show(key){
 	var dataarea=document.getElementById("dataarea");
-	var thevalue=sessionStorage.getItem(key);
-	dataarea.innerHTML="<div>Key: " + key + "--" + "Value: " + thevalue + "</div>";
+	dataarea.innerHTML='<div><button onclick="remove_all()"">Remove all</button></div>';
+	for(i=0;i<sessionStorage.length;i++){
+		var key=sessionStorage.key(i);
+		var thevalue=sessionStorage.getItem(key);
+		dataarea.innerHTML+='<div>' + key + '--' + thevalue + '<br><button onclick="remove_item(\''+key+'\')">Remove</button></div>'}
+	}
+	
+function remove_all(){
+	if(confirm("Are you sure?")){
+		sessionStorage.clear();
+		read_show();
+		}
+	}
+	
+function remove_item(key){
+	if(confirm("Are you sure?")){
+		sessionStorage.removeItem(key);
+		read_show();
+		}
 	}
 
 window.addEventListener("load",start,false);
