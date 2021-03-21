4d183fa4de8f929d6313d24d837f916b692ea430
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-21 18:07:39 +0100

    Update file_js.js

--- file/file_js.js
+++ file/file_js.js
@@ -1,22 +1,32 @@
 //JavaScript Document
 
+var dataarea;
+
 function start(){
 	dataarea=document.getElementById("dataarea");
-	var archives=document.getElementById("archives");
-	archives.addEventListener("change",process,false);
+	var the_files=document.getElementById("the_files");
+	the_files.addEventListener("change",process,false);
 	}
 	
 function process(e){
-	var archives=e.target.files;
-	var my_archive=archives[0];
+	var the_files=e.target.files;
+	dataarea.innerHTML="";
+	var my_file=the_files[0];
+	if(!my_file.type.match(/image/)){
+		alert("Please, select an image");
+		}
+	else{
+		dataarea.innerHTML+="File name: "+my_file.name+"<br>";
+		dataarea.innerHTML+="File size: "+Math.round(my_file.size/1024)+"kb <br>";
+		}
 	var reader=new FileReader();
-	reader.readAsText(my_archive,"iso-8859-1");
+	reader.readAsDataURL(my_file);
 	reader.addEventListener("load",show_info,false);
 	}
 	
 function show_info(e){
-	var result=e.target.result;
-	dataarea.innerHTML=result;
+	var the_result=e.target.result;
+	dataarea.innerHTML+="<img src='"+the_result+"' width='85%'>";
 	}
 
 window.addEventListener("load",start,false);
