7b0a7af6e3dcb6cebdf80e5ce4b34b186beb1c8d
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-03-31 17:00:37 +0200

    Create communication2.php

---
+++ communication/communication2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Communication API II</title>
+</head>
+
+<body>
+<?PHP
+	print("Your name is: ".$_POST["name"]."<br>");
+	print("Your surname is: ".$_POST["surname"]);
+?>
+</body>
+</html>
