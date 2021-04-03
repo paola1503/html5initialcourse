3f6c7b90f475c94c34dd0c649176cc028a0acd6a
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-04-03 13:11:10 +0200

    Update communication4_jsreceiver.js

--- communication/communication4_jsreceiver.js
+++ communication/communication4_jsreceiver.js
@@ -8,6 +8,7 @@ function receiver(e){
 	var messagesarea=document.getElementById("messagesarea");
 	messagesarea.innerHTML+="Message from: " + e.origin + "<br>";
 	messagesarea.innerHTML+="Message: " + e.data + "<br>";
+	e.source.postMessage("The message has been correctly received" +"<br>",e.origin);
 	}
 
 window.addEventListener("load",start,false);
