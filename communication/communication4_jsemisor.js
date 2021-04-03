2b9c5fd9d3939357e18991b4f4ceb43aeb6d0102
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-04-03 13:10:48 +0200

    Update communication4_jsemisor.js

--- communication/communication4_jsemisor.js
+++ communication/communication4_jsemisor.js
@@ -3,12 +3,18 @@
 function start(){
 	var button=document.getElementById("button");
 	button.addEventListener("click",send,false);
+	window.addEventListener("message",receive,false);
+	receiver=document.getElementById("receivingarea");
 	}
 	
 function send(){
 	var message=document.getElementById("message").value;
 	var iframe=document.getElementById("iframe");
 	iframe.contentWindow.postMessage(message,"*");
+	}
+	
+function receive(e){
+	receiver.innerHTML+=e.data;
 	}
 
 window.addEventListener("load",start,false);
