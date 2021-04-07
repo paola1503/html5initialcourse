ba2a1844f107a1369960cdcc50926cc340dc963e
Paola Petri Ortiz <78741678+paola1503@users.noreply.github.com>
2021-04-07 08:38:59 +0200

    Update history_js.js

--- history/history_js.js
+++ history/history_js.js
@@ -4,11 +4,21 @@ function start(){
 	dataarea=document.getElementById("dataarea");
 	url=document.getElementById("url");
 	url.addEventListener("click",changeurl,false);
+	window.addEventListener("popstate",newurl,false);
+	window.history.replaceState(1,null);
 	}
 	
 function changeurl(){
-	dataarea.innerHTML="Welcome to Page 2";
-	window.history.pushState(null,null,"page2.html");
+	show(2);
+	window.history.pushState(2,null,"page2.html");
+	}
+	
+function newurl(e){
+	show(e.state);
+	}
+	
+function show(current){
+	dataarea.innerHTML="This is Page " + current;
 	}
 
 window.addEventListener("load",start,false);
